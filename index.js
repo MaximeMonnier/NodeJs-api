dc63b4ff0e7a755a40dd352d8d8d7912f01379ea
Maxime Monnier <maximemonnier39@gmail.com>
2023-02-03 08:41:48 +0400

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 require('./models/dbConfig');
 const postsRoutes = require('./routes/postsController');
+const bodyParser = require('body-parser');
+const mongoose = require ('mongoose');
+const cors = require('cors')
 
-app.use('/', postsRoutes);
+ mongoose.set('strictQuery', false);
+
+// possible d'autoriser seulement certains site en mettant l'url dans les parametre de cors
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/posts', postsRoutes);
 
 app.listen(5500, () => console.log('server started:5500'));
--- routes/postsController.js
+++ routes/postsController.js
@@ -1,12 +1,68 @@
 const express = require('express');
 const router = express.Router();
+const ObjectID = require('mongoose').Types.ObjectId;
 
 const { PostsModel } = require('../models/postsModel');
 
 router.get('/', (req, res) => {
     PostsModel.find((err, docs) => {
-        console.log(docs);
+        if(!err) res.send(docs);
+        else console.log("Error to get data : " + err)
     })
 })
+
+
+//poster des message dan l'api
+
+router.post('/', (req, res) => {
+    const newRecord = new PostsModel({
+        author: req.body.author,
+        message:  req.body.message
+    });
+    
+    newRecord.save((err, docs) => {
+        if (!err) res.send(docs);
+        else console.log('Error creating new date : ' + err);
+    })
+})
+
+//********************************************************************************** */
+//modifier les données(update)
+
+router.put("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknow : " + req.params.id)
+
+    const updateRecord = {
+        author: req.body.author,
+        message: req.body.message
+    };
+
+    PostsModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: updateRecord },
+        { new: true },
+        (err, docs) => {
+            if(!err) res.send(docs);
+            else console.log("Update error " + err);
+        }
+    )
+})
+//********************************************************************************** */
+//delete
+
+router.delete("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknow : " + req.params.id)
+
+    PostsModel.findByIdAndRemove(
+        req.params.id,
+        (err,docs) => {
+            if(!err) res.send(docs);
+            else console.log("Delete error : " + err);
+        }
+    );
+
+});
 
 module.exports = router
